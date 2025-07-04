52b7408e7639fa5422c69355c8cf61ce3bf7216f
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-07-03 22:03:10 -0300

    feat: finding, typing, and clicking on elements with Cypress - refactored

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -13,26 +13,13 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   });
 
   it('preenche os campos obrigatórios e envia o formulário', () => {
-    // pega um input com id=firstName e digita "Gustavo", depois verifica se o que foi digitado é igual a "gustavo"
-    cy.get('#firstName').type('Gustavo').should('have.value', 'Gustavo');
-
-    // pega um input com o id=lastName e digita "Silva", depois verifica se o que foi digitado é igual a "Silva"
-    cy.get('#lastName').type('Silva').should('have.value', 'Silva');
-
-    // pega um input com o id=email e digita "gustavo.silva@gmail.com", depois verifica se o que foi digitado é igual a "gustavo.silva@gmail.com"
-    cy.get('#email')
-      .type('gustavo.silva@gmail.com')
-      .should('have.value', 'gustavo.silva@gmail.com');
-
-    // pega o textarea com id=open-text-area, digita "Teste", no segundo paramentro do type passa um delay, verifica se o que foi digitado é igual a "Teste"
-    cy.get('#open-text-area')
-      .type('Teste', { delay: 5 })
-      .should('have.value', 'Teste');
-
-    // Pega um botão com o type=submit e clica nele.
+    // Código refatorado
+    cy.get('#firstName').type('Fulano');
+    cy.get('#lastName').type('da Silva');
+    cy.get('#email').type('fulano@hotmail.com');
+    cy.get('#open-text-area').type('Obrigado');
     cy.get('button[type="submit"]').click();
 
-    // Verifica se, após o click, aparece a mensagem de sucesso
     cy.get('.success').should('be.visible');
   });
 });
