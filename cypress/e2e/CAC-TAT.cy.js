82dc202319103383b65df495ae9dbaab98263b49
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-07-07 16:24:33 -0300

    test: select by index

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -111,7 +111,11 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#product').select('YouTube').should('have.value', 'youtube');
   });
 
-  it.only('seleciona um produto (Mentoria) por seu valor (value)', () => {
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
     cy.get('#product').select('mentoria').should('have.value', 'mentoria');
+  });
+
+  it('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product').select(1).should('have.value', 'blog');
   });
 });
